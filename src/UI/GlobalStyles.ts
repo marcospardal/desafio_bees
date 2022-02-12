2614cc44dded725cf82444035ae3594a508534be
Marcos Pardal <marcosnascimento@eng.ci.ufpb.br>
2022-02-12 19:05:40 -0300

    adicionando tema e estilos globais

---
+++ src/UI/GlobalStyles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: "Work Sans";
+    font-weight: 400;
+    font-style: normal;
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+  }
+`
---
+++ src/UI/theme.ts
@@ -0,0 +1,30 @@
+export const theme = {
+  palette: {
+    primary: '#F2EC54',
+    secondary: '#FFFEF0',
+    white: '#FFFFFF',
+    grey: {
+      "500": '#71717A',
+      "600": '#52525B',
+      "700": '#3F3F46',
+      "800": '#27272A'
+    },
+    black: '#000000'
+  },
+  typography: {
+    p: {
+      fontSize: '14px',
+      lineHeight: '21px'
+    },
+
+    p2: {
+      fontSize: '16px',
+      lineHeight: '24px'
+    },
+
+    h1: {
+      fontSize: '30px',
+      lineHeight: '45px'
+    }
+  }
+}
