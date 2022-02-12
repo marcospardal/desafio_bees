9d5570b3405522756398414b5af68c29789c2693
Marcos Pardal <marcosnascimento@eng.ci.ufpb.br>
2022-02-12 19:03:39 -0300

    criando rotas iniciais

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+// views
+import { Home } from './views';
+
+import { theme } from './UI/theme'
+import { GlobalStyle } from './UI/GlobalStyles'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
